4a7860c38313227c1d8d213502c75ba1ebe6caa5
Manu Kumar <m4nukum4r@gmail.com>
2015-02-08 12:32:00 -0800

    stuff happened

--- Web App/js/mappy.js
+++ Web App/js/mappy.js
@@ -1,20 +1,18 @@
-var lat =-25.363882;
-var lng = 131.044922;
-function initialize() {
-    //alert(findLocation(0));
-    var myLatlng = new google.maps.LatLng(lat, lng);
-    var mapOptions = {
-        zoom: 9,
-        center: myLatlng
+var lat = 37.377009;
+var lng = -121.922701;
+    function initialize() {
+        var myLatlng = new google.maps.LatLng(lat,lng);
+        var mapOptions = {
+            zoom: 15,
+            center: myLatlng
+        }
+        var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map,
+            title: 'Caller Location!'
+        });
+        map.setMapTypeId(google.maps.MapTypeId.HYBRID);
     }
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-    var marker = new google.maps.Marker({
-        position: myLatlng,
-        map: map,
-        title: 'Hello World!'
-    });
-    map.setMapTypeId(google.maps.MapTypeId.HYBRID);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initialize());
